c14d479778f801476a6f679f8e752c2dc743e414
Haren Sarma <harensarma@gmail.com>
2022-07-18 00:06:36 +0700

    ss

--- pages/login.js
+++ pages/login.js
@@ -90,6 +90,7 @@ export default class login extends Component {
     try {
       // Will open the MetaMask UI
       // You should disable this button while the request is pending!
+      const { ethereum } = window;
       var address = await ethereum.request({ method: "eth_requestAccounts" });
       return address;
     } catch (error) {
